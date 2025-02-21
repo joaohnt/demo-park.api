df166411db960c06ed12d9e75f377f3e4335713f
joaohnt <jpedrooo.cg@gmail.com>
2025-02-21 13:45:50 -0300

    usuario

---
+++ src/main/java/com/jotas/demo_park/api/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package com.jotas.demo_park.api.service;
+
+import com.jotas.demo_park.api.entity.Usuario;
+import com.jotas.demo_park.api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional(readOnly = true)
+    public Usuario buscarPorId(Long id) {
+        return usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuário não encontrado"));
+    }
+
+    @Transactional
+    public Usuario editarSenha(Long id, String password) {
+        Usuario user = buscarPorId(id);
+        user.setPassword(password);
+        return user;
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> buscarTodos() {
+        return usuarioRepository.findAll();
+    }
+}
